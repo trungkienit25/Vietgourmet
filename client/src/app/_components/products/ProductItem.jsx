520544fb9c2f3f2b2a7ed11cc4c9b943d15ee699
trungkienit25 <162586320+trungkienit25@users.noreply.github.com>
2024-10-21 16:27:01 +0700

    Update ProductItem.jsx

--- client/src/app/_components/products/ProductItem.jsx
+++ client/src/app/_components/products/ProductItem.jsx
@@ -69,7 +69,7 @@ const ProductItem = ({ item, index, marginBottom, moreType }) => {
         </Link>
         <div className="sb-card-tp">
           <h4 className="sb-card-title"><Link href={`/product`}>{item.title}</Link></h4>
-          <div className="sb-price"><sub>{item.currency}</sub> {item.price}</div>
+          <div className="sb-price"> {item.price}  <sub>{item.currency}</sub></div>
         </div>
         <div className="sb-description">
           <p className="sb-text sb-mb-15">
